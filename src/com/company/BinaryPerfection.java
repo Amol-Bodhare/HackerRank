d59eb9421535d62244d41ae786c35f43a5e6e2a1
unknown <amolfeb95@gmail.com>
2018-03-13 02:09:50 -0400

    Final Commit

--- src/com/company/BinaryPerfection.java
+++ src/com/company/BinaryPerfection.java
@@ -2,10 +2,18 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class BinaryPerfection {
     public static void main(String[] args) {
-        int[] arr=new int[7];
+        Scanner sc=new Scanner(System.in);
+        int height=sc.nextInt();
+        int numberOfNodes=sc.nextInt();
+        int[] nodes=new int[numberOfNodes];
+        for(int i=0;i<nodes.length;i++)
+            nodes[i]=sc.nextInt();
+
+        int[] arr=new int[(int) ((Math.pow(2, height)-1))];
         String temp="00";
         arr[0]=0;
         arr[1]=0;
@@ -26,8 +34,22 @@ public class BinaryPerfection {
             }
         }
 
-        System.out.println(Arrays.toString(arr));
+        for(int i=0;i<nodes.length;i++) {
+            int tempInt=arr[nodes[i]-1];
+            for(int j=nodes[i]-1;j<arr.length;j++){
+                if(j==arr.length-1) {
+                    nodes[i] = -1;
+                    break;
+                }
+                if(arr[j+1]>tempInt) {
+                    nodes[i] = j + 2;
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(nodes));
     }
+
     public static boolean check(String temp, int[] arr){
         int count=0;
         for(int i=0;i<temp.length();i++){
